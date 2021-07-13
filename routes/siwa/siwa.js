767d97c9592bbefd08b424b0ce65fb86c3d12902
Cameron Richards <crichards1717@gmail.com>
2021-07-12 22:39:46 -0700

    fixed redirectUri for test env

--- routes/siwa/siwa.js
+++ routes/siwa/siwa.js
@@ -8,14 +8,14 @@ router.post('/', async (req, res) => {
 
   const options = {
     clientID: process.env.CLIENT_ID,
-    redirectUri: 'https://splopple-music.herokuapp.com/applelogin',
+    redirectUri: 'https://splopple-siwa-test.herokuapp.com/applelogin',
     clientSecret: process.env.CLIENT_SECRET
   }
 
   try {
     tokenResponse = await appleSignin.getAuthorizationToken(code, {
       clientID: 'com.n8blake.splopple-music',
-      redirectUri: 'https://splopple-music.herokuapp.com/applelogin',
+      redirectUri: 'https://splopple-siwa-test.herokuapp.com/applelogin',
       clientSecret: 'eyJraWQiOiIyTUg1QjY5NFA1IiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJUNFM2UjNZMzlHIiwiaWF0IjoxNjI1OTQ2MzMxLCJleHAiOjE2NDE0OTgzMzEsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJjb20ubjhibGFrZS5zcGxvcHBsZS1tdXNpYyJ9.CiahQv9eR45-YBKACHgWoprP5-nlEm1ORKR2HlK-wqValWOMDGx78hYVxK8ID5hlBNjVYKMMpAfMqWZgMhkUQw'
     });
   } catch (err) {
